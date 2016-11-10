1bd367d4d127bc98f4fd11642135c4ef03e77abe
David Patiashvili <stratagem.david@gmail.com>
2016-11-10 20:04:18 +0100

    Tests: Fix invalid run

--- tests/run.sh
+++ tests/run.sh
@@ -5,7 +5,7 @@ echo "Download last composer.phar"
 php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
 php composer-setup.php
 ./composer.phar clear-cache
-./composer install --dev
+./composer.phar install --dev
 cd -
 
 ../vendor/bin/phpunit --coverage-clover build/logs/clover.xml
